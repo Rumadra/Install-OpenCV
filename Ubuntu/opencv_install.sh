05164df992f0c4f7e4df98852b7c09de18f808f9
Rumadra <31647014+Rumadra@users.noreply.github.com>
2023-09-06 01:54:03 +0900

    Update opencv_install.sh

--- Ubuntu/opencv_install.sh
+++ Ubuntu/opencv_install.sh
@@ -16,10 +16,10 @@ fi
 if [[ -z "$dldir" ]]; then
     dldir=OpenCV
 fi
-if ! sudo true; then
-    echo "You must have root privileges to run this script."
-    exit 1
-fi
+# if ! sudo true; then
+#     echo "You must have root privileges to run this script."
+#     exit 1
+# fi
 set -e
 
 echo "--- Installing OpenCV" $version
@@ -44,7 +44,10 @@ mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
 make -j 4
-sudo make install
-sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
-sudo ldconfig
+# sudo make install
+make install
+# sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+# sudo ldconfig
+ldconfig
 echo "OpenCV" $version "ready to be used"
